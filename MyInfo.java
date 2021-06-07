e1f8990773d61b195b2882c33ddc734d3bbcd858
teerjustin <teerjustin@gmail.com>
2021-06-07 14:01:25 -0700

    initial commit

---
+++ MyInfo.java
@@ -0,0 +1,7 @@
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("My name is Justin Teer");
+        System.out.println("I am 21 years old.");
+        System.out.println("My hometown is San Diego, California");
+    }
+}
